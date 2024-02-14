99e9053878842cb42e3af21f59e1272de524e23b
Uway C <chung00818@gmail.com>
2024-02-14 01:23:02 -0800

    updated

--- index.js
+++ index.js
@@ -7,10 +7,16 @@ const dotenv = require("dotenv");
 dotenv.config();
 app.use(express.json());
 
+//when not using Docker
+//let corspolicy = {
+//    origin:"http://localhost:3000"
+//}
 
-let corspolicy = {
-    origin:"http://localhost:3000"
-}
+//For clone and pull to Docker, a Dockerfile specifies the path
+ let corspolicy = {
+     origin: process.env.FrontendURI
+ }
+
 app.use(cors(corspolicy));
 
 
@@ -22,7 +28,6 @@ app.use((req,res,next) => {
 // schema
 // model
 // from middleware, using model to get data from DB
-
 
 //connect
 let db = async () => {
@@ -41,7 +46,7 @@ db();
 
 //connect,  old style - hard coded path
 // let db = async () => {
-//     try{ 
+//     try{
 //         await mongoose.connect("mongodb+srv://honorhomesolutions:Yytt@cluster0.cw2ygsf.mongodb.net/realgrande?retryWrites=true&w=majority");
 //          //await mongoose.connect("mongodb+srv://procareer:pro1234@cluster0.mj1t2ck.mongodb.net/realgrande?retryWrites=true&w=majority");
 //          console.log(" connected to database");
@@ -65,9 +70,87 @@ app.use('/',allroutes);
 
 
 
-
-
-
-
-
 app.listen(4000,()=>{ console.log("Backend server listening at port 4000")});
+ 
+
+
+// let express = require('express');
+// let app = express();
+// let allroutes = require('./routes/AllRoutes');
+// const mongoose = require('mongoose');
+// const cors = require('cors')
+// const dotenv = require("dotenv");
+// dotenv.config();
+// app.use(express.json());
+
+
+// // let corspolicy = {
+// //    origin:"http://localhost:3000"
+// // }
+
+
+// //For clone and pull to Docker, a Dockerfile specifies the path
+// let corspolicy = {
+//     origin: process.env.FrontendURI
+// }
+
+// app.use(cors(corspolicy));
+
+
+// app.use((req,res,next) => {
+//     console.log(" Request received at " + (new Date()));
+//     next();
+// });
+// // connect to the database
+// // schema
+// // model
+// // from middleware, using model to get data from DB
+
+
+// //connect
+// let db = async () => {
+//     try{
+//         console.log(process.env.DBURI);
+//         await mongoose.connect(process.env.DBURI);
+//         console.log(" connected to database");
+//     }
+//     catch(err) {
+//         console.log(' error connecting');
+//         res.status(500).send(err);
+//     }
+// }
+// db();
+
+
+// //connect,  old style - hard coded path
+// // let db = async () => {
+// //     try{ 
+// //         await mongoose.connect("mongodb+srv://honorhomesolutions:Yytt@cluster0.cw2ygsf.mongodb.net/realgrande?retryWrites=true&w=majority");
+// //          //await mongoose.connect("mongodb+srv://procareer:pro1234@cluster0.mj1t2ck.mongodb.net/realgrande?retryWrites=true&w=majority");
+// //          console.log(" connected to database");
+// //     }
+// //     catch(err) {
+// //         console.log(' error connecting');
+// //         res.status(500).send(err);
+// //     }
+// // }
+// // db();
+
+
+
+// app.use('/',allroutes);
+
+
+// // connect to the database
+// // schema
+// // model
+// // from middleware, use model to get data from DB
+
+
+
+
+
+
+
+
+// app.listen(4000,()=>{ console.log("Backend server listening at port 4000")});
